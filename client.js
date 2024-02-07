d2554d3d8aa438a3443841e360f7799a4ebb2a8f
kimal <kim.alexander.ca@gmail.com>
2024-02-07 12:49:06 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,260 @@
+const configuration = {iceServers: [{urls: 'stun:stun.l.google.com:19302'}]};
+const constraints = {video: true, audio: true};
+
+const localVideo = document.getElementById('localVideo');
+const remoteVideosContainer = document.getElementById('remoteVideos');
+let sessionID = null;
+let roomId = 'test-room';
+let localStream;
+const peerConnections = {};
+
+navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+        localStream = stream;
+        localVideo.srcObject = stream;
+        startSignaling();
+    })
+    .catch(error => {
+        console.error('Error accessing media devices:', error);
+    });
+
+function createPeerConnection(peerId) {
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    localStream.getTracks().forEach(track => {
+        console.log('peerId', peerId, 'track', track);
+        peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.ontrack = event => {
+        if (document.getElementById('video-' + peerId) === null) {
+            const remoteVideoFrame = document.createElement('div');
+            const remoteVideo = document.createElement('video');
+            remoteVideo.srcObject = event.streams[0];
+            remoteVideo.autoplay = true;
+            remoteVideo.classList.add('play');
+            remoteVideo.muted = false;
+            remoteVideoFrame.id = 'video-' + peerId;
+            remoteVideoFrame.setAttribute('alt', peerId);
+            remoteVideoFrame.style="display:inline-block;float:left;";
+            // remoteVideo.style = "";
+            remoteVideoFrame.appendChild(remoteVideo);
+            muteAudio = document.createElement('button');
+            muteAudio.textContent = '🔇Mute' ;
+            muteAudio.onclick = function () {
+                this.parentElement.querySelector('video').muted = !this.parentElement.querySelector('video').muted;
+                this.textContent = this.parentElement.querySelector('video').muted ? '🔈Unmute' : '🔇Mute' ;
+                txt = this.parentElement.querySelector('video').muted ? 'Unmute' : 'Mute' ;
+                this.setAttribute('alt', txt);
+                this.setAttribute('tooltip', txt);
+                this.parentElement.querySelector('video').classList.toggle('muted')
+            };
+            remoteVideoFrame.appendChild(muteAudio);
+
+            muteVideo = document.createElement('button');
+            muteVideo.textContent = 'Stop' ;
+            muteVideo.onclick = function () {
+                video = this.parentElement.querySelector('video');
+                if (video.classList.contains('play')) {
+                    txt = 'Play'; // : 'Stop' ;
+                    stream = video.srcObject;
+                    tracks = stream.getTracks();
+
+                    tracks.forEach((track) => {
+                        if (track.kind === 'video') track.enabled = false; //stop();
+                    });
+                } else {
+                    txt = 'Stop' ;
+                    stream = video.srcObject;
+                    tracks = stream.getTracks();
+
+                    tracks.forEach((track) => {
+                        if (track.kind === 'video') track.enabled = true; //stop();
+                    });
+                    //this.stream.removeTrack(this.stream.getVideoTracks()[0])
+                    // video.srcObject.getTracks().forEach(track => {
+                    //     if (track.kind === 'video') {
+                    //         video.stream.addTrack(track);
+                    //     }
+                    // });
+
+
+                }
+                this.textContent = txt;
+                this.setAttribute('alt', txt);
+                this.setAttribute('tooltip', txt);
+                this.parentElement.querySelector('video').classList.toggle('play');
+
+            };
+            remoteVideoFrame.appendChild(muteVideo);
+
+
+            remoteVideosContainer.appendChild(remoteVideoFrame);
+        }
+    };
+
+    peerConnections[peerId] = peerConnection;
+    return peerConnection;
+}
+
+function createRoom() {
+    const ws = new WebSocket('wss://video.ttl10.net:3000'); // Replace with your WebSocket server
+    let roomName = document.getElementById('roomName').value;
+
+    ws.send(JSON.stringify({type: 'create-room', 'name': roomName, host: sessionID}));
+
+}
+
+function removePeerConnection(id) {
+    delete peerConnections[id];
+    document.getElementById('video-'+id).outerHTML = "";
+    // document.createElement('video')
+    //document.getElementById('remoteVideos').innerHTML = '';
+
+}
+
+function startSignaling() {
+    const ws = new WebSocket('wss://video.ttl10.net:3000'); // Replace with your WebSocket server
+
+    ws.onopen = () => {
+        ws.send(JSON.stringify({type: 'join', roomId: roomId}));
+    };
+
+    ws.onmessage = event => {
+        const data = JSON.parse(event.data);
+        if (data.type === 'id') {
+            // Assign the unique ID received from the server
+            const clientId = data.id;
+            sessionID = clientId;
+
+            console.log('You are joined as:', clientId);
+
+            if(sessionID !== data.room.host.uid) {
+                peerConnection = createPeerConnection(data.room.host.uid);
+                peerConnection.onicecandidate = event => {
+                    if (event.candidate) {
+                        sendIceCandidate(sessionID, data.room.host.uid, event.candidate);
+                    }
+                };
+                sendOffer(ws, clientId, data.room.host.uid, peerConnection);
+                for(i = 2; i <= data.room.size; i++) {
+                    user = data.room['u'+(i-1)];
+                    if (user.uid !== sessionID) {
+                        peerConnection = createPeerConnection(user.uid);
+                        peerConnection.onicecandidate = event => {
+                            if (event.candidate) {
+                                sendIceCandidate(sessionID, user.uid, event.candidate);
+                            }
+                        };
+                        sendOffer(ws, clientId, user.uid, peerConnection);
+                    }
+                }
+            }
+
+        } else if (data.type === 'participant-joined') {
+            // Handle new participant joined
+            const clientId = data.id;
+            if (sessionID != clientId) {
+                peerConnection = createPeerConnection(clientId);
+                peerConnection.onicecandidate = event => {
+                    if (event.candidate) {
+                        sendIceCandidate(sessionID, clientId, event.candidate);
+                    }
+                };
+                console.log('Participant joined:', clientId);
+            }
+
+        } else if (data.type === 'participant-left') {
+            // Handle participant left
+            console.log('Participant left:', data.id);
+            removePeerConnection(data.id);
+        } else if (data.type === 'offer') {
+            console.log('Offer:', data.id, data.description);
+            handleOffer(data);
+        } else if (data.type === 'participant-offer') {
+            const clientId = data.from;
+            if (sessionID != clientId) {
+                console.log('Offer:', data.from, data.description);
+                handleOffer(data);
+            }
+        } else if (data.type === 'answer') {
+            handleAnswer(data);
+        } else if (data.type === 'ice-candidate') {
+            handleIceCandidate(data);
+        } else if (data.type === 'room-list') {
+            handleRoomList(data);
+        }
+    };
+
+    function handleOffer(offer) {
+        const peerConnection = peerConnections[offer.from];
+        if (peerConnection == undefined) {
+            console.error('Peer  connection ', offer.from, 'undefined');
+        } else {
+            peerConnection.setRemoteDescription(new RTCSessionDescription(offer.description));
+            peerConnection.createAnswer()
+                .then(answer => peerConnection.setLocalDescription(answer))
+                .then(() => {
+                    sendAnswer(offer.from, peerConnection.localDescription);
+                })
+                .catch(error => {
+                    console.error('Error creating answer:', error);
+                });
+        }
+
+    }
+
+    function handleAnswer(answer) {
+        const peerConnection = peerConnections[answer.from];
+
+        peerConnection.setRemoteDescription(new RTCSessionDescription(answer.description));
+    }
+
+    function handleRoomList(data) {
+        select = document.getElementById('roomList');
+        select.innerHTML = '';
+        roomlist = data.room - list;
+        roomlist.forEach(room => {
+            var opt = document.createElement('option');
+            opt.innerHTML = room.name;
+            opt.value = room.host;
+            select.appendChild(opt);
+        })
+
+    }
+
+    function handleIceCandidate(candidate) {
+        const peerConnection = peerConnections[candidate.from];
+        peerConnection.addIceCandidate(new RTCIceCandidate(candidate.candidate));
+    }
+
+    async function sendOffer(ws, from, to, peerConnection) {
+        const offerOptions = {
+            offerToReceiveAudio: 1,
+            offerToReceiveVideo: 1
+        };
+        const offer = await peerConnection.createOffer(offerOptions);
+        await peerConnection.setLocalDescription(offer);
+        peerConnection.onicecandidate = event => {
+            if (event.candidate) {
+                sendIceCandidate(from, to, event.candidate);
+            }
+        };
+        // ws.send({
+        //     type: 'offer',
+        //     data: offer
+        // });
+        console.log('Peer connection created:', from, to, peerConnection, offer);
+        console.log('sessionID :', sessionID);
+        ws.send(JSON.stringify({type: 'offer', from, to, offer}));
+    }
+
+    function sendAnswer(to, description) {
+        console.log('answer sessionID :', sessionID);
+        ws.send(JSON.stringify({type: 'answer', to, from: sessionID, description}));
+    }
+
+    function sendIceCandidate(from, to, candidate) {
+        ws.send(JSON.stringify({type: 'ice-candidate', from, to, candidate}));
+    }
+}
---
+++ signaling-ws.js
@@ -0,0 +1,230 @@
+const WebSocket = require('ws');
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('video.db');
+
+db.serialize(() => {
+    db.prepare(`CREATE TABLE IF NOT EXISTS rooms (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, room TEXT NOT NULL )`).run().finalize();
+    var n = 0;
+    db.all(`SELECT * FROM rooms`, (err, rows) => {
+
+        rows.forEach(function (row) {
+            n++;
+            console.log(row)
+        });
+        if (n===0) {
+            db.run('INSERT INTO rooms(name, room) VALUES(?, ?)', ['test-room1','{}'], (err) => {
+                if(err) {
+                    return console.log(err.message);
+                }
+                console.log("Row was added to the table: ", db.lastID);
+            })
+        }
+    })
+
+    db.close();
+});
+
+
+const app = express();
+
+// Load SSL certificate and private key
+const privateKey = fs.readFileSync('video-key.pem', 'utf8');
+const certificate = fs.readFileSync('video-cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const server = https.createServer(credentials, app);
+const wss = new WebSocket.Server({ server });
+
+const clients = {};
+const clientOffers = {};
+const rooms = {};
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        const data = JSON.parse(message);
+
+        switch (data.type) {
+            case 'join':
+                handleJoin(ws, data);
+                break;
+            case 'offer':
+                handleOffer(ws, data);
+                break;
+            case 'answer':
+                handleAnswer(ws, data);
+                break;
+            case 'ice-candidate':
+                handleIceCandidate(ws, data);
+                break;
+            case 'create-room':
+                handleCreateRoom(ws, data);
+                break;
+            default:
+                console.error('Unknown message type:', data.type);
+        }
+    });
+
+    ws.on('close', () => {
+        removeClient(ws);
+    });
+});
+
+function handleJoin(ws, data) {
+    const clientId = generateClientId();
+    roomId = data.roomId;
+    var roomFile = 'rooms/'+roomId+'.json';
+    var obj = {};
+    fs.readFile(roomFile, 'utf8', function (err, data) {
+        if (err) {
+            obj['host'] = { uid: clientId };
+            obj['size'] = 1;
+        } else {
+            obj = JSON.parse(data);
+            obj['u'+obj.size] = { uid: clientId };
+            obj.size = obj.size + 1;
+
+        }
+
+        clients[clientId] = ws;
+        ws.uid = clientId;
+
+        // Send the new client their ID
+        ws.send(JSON.stringify({ type: 'id', id: clientId, room: obj, test: "test : " }));
+        console.log('wsid: ',ws.uid,clientId, 'obj :',  JSON.stringify(obj));
+
+        fs.writeFileSync(roomFile, JSON.stringify(obj) , 'utf-8');
+
+
+        // Notify other clients about the new participant
+        broadcast(JSON.stringify({ type: 'participant-joined', id: clientId }));
+    });
+
+
+    // Send offers to existing clients
+    // Object.keys(clients).forEach(id => {
+    //     if (id !== clientId) {
+    //
+    //         sendOffer(ws, id, offerDescription);
+    //     }
+    // });
+}
+
+function handleOffer(sender, data) {
+    const recipient = clients[data.to];
+    console.log('Offer from : ',sender.uid, ' to: ',data.to);
+    if (recipient) {
+        recipient.send(JSON.stringify({ type: 'participant-offer', from: data.from, description: data.offer }));
+    }
+
+
+
+    // if (recipient) {
+    //     recipient.send(JSON.stringify({ type: 'offer', from: sender.id, description: data.description }));
+    // }
+}
+
+function handleCreateRoom(sender, data) {
+    rooms.push({ name: data.name, host: data.host})
+
+    broadcast(JSON.stringify({ type: 'room-list', roomList: rooms }));
+
+}
+
+function handleAnswer(sender, data) {
+    const recipient = clients[data.to];
+
+
+    if (recipient) {
+        recipient.send(JSON.stringify({ type: 'answer', from: data.from, description: data.description }));
+    }
+}
+
+function handleIceCandidate(sender, data) {
+    const recipient = clients[data.to];
+
+    if (recipient) {
+        // recipient.send(JSON.stringify({ type: 'ice-candidate', from: data.from, candidate: data.candidate }));
+        recipient.send(JSON.stringify({ type: 'ice-candidate', from: data.from, candidate: data.candidate }));
+    }
+}
+
+function sendOffer(sender, recipientId, offerDescription) {
+    const recipient = clients[recipientId];
+
+    if (recipient) {
+        sender.send(JSON.stringify({ type: 'offer', to: recipientId, description: offerDescription }));
+    }
+}
+
+function broadcast(message) {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
+function removeClient(ws) {
+    const clientId = ws.uid;
+    var roomFile = 'rooms/'+'test-room'+'.json';
+    fs.readFile(roomFile, 'utf8', function (err, data) {
+        obj = JSON.parse(data);
+        if (clientId) {
+            delete clients[clientId];
+            removeId = 0;
+            if (obj.host.uid === clientId) {
+                if (obj.hasOwnProperty('u1')) {
+                    obj.host = obj.u1;
+                    removeId = 1;
+                    obj.size = obj.size - 1;
+                    console.log('183 size:', obj.size);
+                }
+            } else {
+                for (let i= 1; i <= 10; i++) {
+                    if (obj.hasOwnProperty('u'+i)) {
+                        if (obj['u'+i].uid === clientId) {
+                            delete obj['u'+i];
+                            removeId = i;
+                            obj.size = obj.size - 1;
+                            console.log('192 size:', obj.size);
+                            break;
+                        }
+                    }
+                }
+            }
+            for (let i= removeId; i <= 10; i++) {
+                if (obj.hasOwnProperty('u'+(i+1))) {
+                    obj['u'+i] = obj['u'+(i+1)];
+                    delete obj['u'+(i+1)];
+                    console.log('202 size:', obj.size, 'remove id: ', removeId);
+                    //obj.size = obj.size - 1;
+                }
+            }
+            fs.writeFileSync(roomFile, JSON.stringify(obj) , 'utf-8');
+            // Notify other clients about the departure
+            broadcast(JSON.stringify({ type: 'participant-left', id: clientId, room: obj }));
+        }
+    });
+
+}
+
+function getClientId(ws) {
+    for (const clientId in clients) {
+        if (clients[clientId] === ws) {
+            return clientId;
+        }
+    }
+    return null;
+}
+
+function generateClientId() {
+    return Math.random().toString(36).substr(2, 9);
+}
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
