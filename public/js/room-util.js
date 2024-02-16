bf814b11f67b3437e3fa52e5768a43ba4ced05fa
Mikaniri <1irinakim1@gmail.com>
2024-02-16 12:11:06 -0500

    flex size of videos

--- public/js/room-util.js
+++ public/js/room-util.js
@@ -29,9 +29,9 @@ function createPeerConnection(peerId) {
         if (document.getElementById('video-' + peerId) === null) {
             remoteVideoFrame = createRemoteVideo(peerId, event.streams[0]);
             remoteVideosContainer.appendChild(remoteVideoFrame);
+            adjustVideoSize();
         }
     };
-
     peerConnections[peerId] = peerConnection;
     return peerConnection;
 }
@@ -116,5 +116,23 @@ function removePeerConnection(id) {
 
 function adjustVolume(video, volume) {
     video.volume = volume;
+}
+
+function adjustVideoSize(){
+    // Get the container and items
+    const items = Array.prototype.slice.call(document.getElementsByTagName('video'), 0);
+    // Calculate the width of each item based on the number of items
+    if (items.length >= 5){
+        var itemWidth = window.innerWidth / 5 - 2 * 5; // subtracting margin twice (left and right)
+    } else {
+        var itemWidth = window.innerWidth / items.length - 2 * 5; // subtracting margin twice (left and right)
+    }
+    var itemHeight = itemWidth * 0.75;
+    // Set the width for each item
+    items.forEach(item => {
+        item.style.width = `${itemWidth}px`;
+        item.style.height = `${itemHeight}px`;
+    });
+
 }
 
